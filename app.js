2c3dd4e2f48bd4fdd85373e203d5480363224f17
EmmaHogberg <70634183+EmmaHogberg@users.noreply.github.com>
2020-10-15 22:42:57 +0200

    Fetch animal finished

--- app.js
+++ app.js
@@ -1,16 +1,51 @@
-const selectAnimal = document.querySelector("#select-animal");
-const showAnimal = document.querySelector("#show-animal");
+const animalType = document.querySelector("#select-animal");
+const showAnimalButton = document.querySelector("#show-animal");
+const currentImage = document.querySelector("img");
+const favContainer = document.querySelector(".favContainer");
 
 const dogInputApi = "https://dog.ceo/api/breeds/image/random";
 const foxInputApi = "https://randomfox.ca/floof/";
 const catInputApi = "https://api.thecatapi.com/v1/images/search?";
+
+// Listen to show animal button press
+showAnimalButton.addEventListener("click", getAnimalType);
+
+// Get user input on choice of animal type and fetch image
+function getAnimalType(element) {
+  element = animalType.value;
+
+  switch (element) {
+    case "dog":
+      getDog();
+      break;
+
+    case "fox":
+      getFox();
+      break;
+
+    case "cat":
+      getCat();
+      break;
+
+    default:
+      animalType.className = "select-box error";
+  }
+}
+
+// Remove error after drop-down change
+animalType.addEventListener("change", () => {
+  if (animalType.value !== "select") {
+    animalType.className = "select-box";
+  }
+});
 
 // Get picture of random dog
 function getDog() {
   fetch(dogInputApi)
     .then((response) => response.json())
     .then((json) => {
-      document.querySelector("#image").src = json.message;
+      currentImage.src = json.message;
+      // document.querySelector("img").id = "image";
     });
 }
 
@@ -19,7 +54,8 @@ function getFox() {
   fetch(foxInputApi)
     .then((response) => response.json())
     .then((json) => {
-      document.querySelector("#image").src = json.image;
+      currentImage.src = json.image;
+      // document.querySelector("img").id = "image";
     });
 }
 
@@ -29,7 +65,22 @@ function getCat() {
     .then((response) => response.json())
     .then((json) => {
       console.log(json[0].url);
-      document.querySelector("#image").src = json[0].url;
+      currentImage.src = json[0].url;
+      // document.querySelector("img").id = "image";
     });
 }
-getDog();
+
+// Listen to click on current animal image
+currentImage.addEventListener("click", saveAnimal);
+
+// Save current animal image to favorie
+function saveAnimal() {
+  const newImage = document.createElement("img");
+  newImage.src = currentImage.src;
+
+  const newDiv = document.createElement("div");
+  newDiv.className = "favItem";
+  newDiv.appendChild(newImage);
+
+  favContainer.appendChild(newDiv);
+}
